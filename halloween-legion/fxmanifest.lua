2b8ec3ce9bb2f74d1bb77bce04e7b56b73eb1348
nzkfc <nzryancas@gmail.com>
2023-08-08 21:22:59 +1200

    Add files via upload

---
+++ halloween-legion/fxmanifest.lua
@@ -0,0 +1,51 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'nzkfc'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+    "stream/*.ymap",
+    "stream/*.ytd",
+    "stream/props/*.ydr",
+    "stream/*.ybn",
+    "stream/props/*.ytyp", 
+    "stream/*.ymf",
+    "stream/*.yft",
+}
+
+data_file "DLC_ITYP_REQUEST" "4tombstones_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "20gravestones_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "angelofdeath_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "blackspider_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "catreaper_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "cauldrons_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "cuteghost_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "deathghost_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "halloweencandle_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "jackolantern_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "medichandelier_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "medcoffin_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "oldcoffin_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "pumpkinreal_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "scarycapes_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "stonecross_ytyp.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "whiteskull_ytyp.ytyp"
